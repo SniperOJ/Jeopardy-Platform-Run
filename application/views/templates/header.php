53d4eec1ae0f93a27dd8eeda7d34d7b585646937
WangYihang <wangyihanger@gmail.com>
2017-04-23 01:38:42 +0800

    formate

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -8,12 +8,12 @@
 	<meta name="keywords" content="SniperOJ,CTF">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<link href="https://cdn.bootcss.com/tether/1.4.0/css/tether.min.css" rel="stylesheet">
-	<link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="https://cdn.bootcss.com/tether/1.4.0/css/tether.min.css">
+	<link rel="stylesheet" type="text/css" href="https://cdn.bootcss.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
 
 	<title>SniperOJ</title>
 
-	<script src="https://cdn.bootcss.com/tether/1.4.0/js/tether.min.js"></script>
+	<script type="text/javascript" src="https://cdn.bootcss.com/tether/1.4.0/js/tether.min.js"></script>
 	<script type="text/javascript" src="https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js"></script>
 	<script type="text/javascript" src="https://cdn.bootcss.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"></script>
 
