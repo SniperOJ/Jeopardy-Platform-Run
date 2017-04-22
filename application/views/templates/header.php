2ca55714b54620fb93ad3e81f7b306056a77dc92
WangYihang <wangyihanger@gmail.com>
2017-04-23 01:36:13 +0800

    remove usless files

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -10,14 +10,12 @@
 
 	<link href="https://cdn.bootcss.com/tether/1.4.0/css/tether.min.css" rel="stylesheet">
 	<link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
-	<link rel="stylesheet" href="https://cdn.bootcss.com/amazeui/2.7.2/css/amazeui.min.css">
 
 	<title>SniperOJ</title>
 
 	<script src="https://cdn.bootcss.com/tether/1.4.0/js/tether.min.js"></script>
 	<script type="text/javascript" src="https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js"></script>
 	<script type="text/javascript" src="https://cdn.bootcss.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="https://cdn.bootcss.com/amazeui/2.7.2/js/amazeui.min.js"></script>
 
 </head>
 <body>
