0811c1a7ccbb673593182945330eb54aa92021a7
WangYihang <wangyihanger@gmail.com>
2017-04-24 03:23:57 +0800

    adjust

--- application/views/home/content.php
+++ application/views/home/content.php
@@ -10,12 +10,12 @@
         <div id="nav">
             <h2 id="title"><i class="fa fa-sitemap"></i>SniperOJ</h2>
             <ul id="toggle">
-                <li>
+<!--                 <li>
                     <div class="active border">
                         <span class="menu-icons fa fa-home"></span>   <a href="#">我的主页</a>
                     </div>
-                </li>
-                <li>
+                </li> -->
+<!--                 <li>
                     <div>
                         <span class="menu-icons  fa fa-user"></span>   <a href="#">题目</a>
                         <span class="the-btn fa fa-plus"></span>
@@ -40,11 +40,11 @@
                             <a href="#">Web</a>
                         </li>
                     </ul>
-                </li>
+                </li> -->
                 <li>
                     <div>
                         <span class="menu-icons  fa fa-briefcase"></span>
-                        <a href="#">WriteUp</a><span class="the-btn fa fa-plus"></span>
+                        <a href="#">write-up</a><span class="the-btn fa fa-plus"></span>
                     </div>
                     <ul>
                         <li>
@@ -58,7 +58,7 @@
                 <li>
                     <div>
                         <span class="menu-icons  fa fa-briefcase"></span>
-                        <a href="#">Wiki</a><span class="the-btn fa fa-plus"></span>
+                        <a href="#">Tutorial</a><span class="the-btn fa fa-plus"></span>
                     </div>
                     <ul>
                         <li>
@@ -68,17 +68,6 @@
                             <a href="#">Misc</a>
                         </li>
                     </ul>
-                </li>
-                <li>
-                    <div class="active border">
-                        <span class="menu-icons fa fa-home"></span>   <a href="#">退出登录</a>
-                    </div>
-                </li>
-                <li>
-                    <div>
-                        <span class="menu-icons  fa fa-envelope"></span>
-                        <a href="#">关于我们</a>
-                    </div>
                 </li>
                 <li class="cd-login">
                     <div>
@@ -95,11 +84,6 @@
             </ul>
         </div>
         <a href="#" class="toggle-nav" id="bars"><i class="fa fa-bars"></i></a>
-
-
-
-
-
 
         <div class="challenges-container">
             <h1>All challenges</h1>
@@ -124,7 +108,6 @@
 
 <script type="text/javascript" src="/assets/js/sliding.js"></script>
 <script type="text/javascript" src="/assets/js/blocks.js"></script>
-
 
 <script type="text/javascript">
     /* 动态加载所有题目 */
@@ -196,3 +179,6 @@
 </div>  
 
 <script type="text/javascript" src="/assets/js/show_alert_dialog.js"></script>
+
+
+
