0952d3ebae67965df1a66c91232895652519d871
WangYihang <wangyihanger@gmail.com>
2017-04-24 21:44:47 +0800

    pass rate

--- application/views/slide_bar/content_user.php
+++ application/views/slide_bar/content_user.php
@@ -812,11 +812,11 @@
                 // html += '通过率 : '+user_data.pass_rate;
                 var user_data = rank_data[i]
                 var html = ''
-                html +=  '<tr class="hint--left" aria-label="' + user_data.college + '">';
+                html +=  '<tr class="hint--left" aria-label="' + '学校 : ' + user_data.college + '<br>通过率 : ' + user_data.pass_rate + '">';
                 html += '<td>' + (i + 1) + '</td>';
                 html += '<td>' + user_data.username + '</td>';
-                html += '<td>' + user_data.score + '</td>';
-                html += '</tr><br>'
+                html += '<td>' + user_data.score + ' ' + ' (' + user_data.accept_times + ' / ' + user_data.submit_times + ')' + '</td>';
+                html += '</tr>' 
                 rank_tbody.append(html);
               }
             }
