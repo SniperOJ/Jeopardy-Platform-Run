77d5457e998a4f48105e6196636abc0ce0e566c3
WangYihang <wangyihanger@gmail.com>
2017-04-26 14:35:37 +0800

    add home

--- application/views/home/content.php
+++ application/views/home/content.php
@@ -1,22 +1,19 @@
 <style type="text/css">
 	.content-container{
 		text-align: center;
-		margin-top: 196px;
+		margin-top: 192px;
 		font-size: 32px;
 	}
 </style>
-
+<link href="//cdn.bootcss.com/hint.css/2.5.0/hint.min.css" rel="stylesheet">
 <div id="main-scrollbar" class="content-container" data-scrollbar>
-	<h1 class="cover-heading">Capture The Flag</h1>
+	<h1 class="cover-heading"><span class="hint--right" aria-label="Fork/Star On GitHub"><a target="_blank" href="https://github.com/WangYihang/SniperOJ">SniperOJ</a></span></h1>
 	<p class="lead">
-		SniperOJ is an open source and free CTF online judge system<br>
-		This system is currently under development now<br>
-		if you have any suggestions or comments or questions<br>
-		you can join the QQ group (514782018) to discuss<br>
-		or click the email button below the page to contact with me<br>
-		And your question will be quickly processed.<br>
-	<a href="https"></a>
-	<p class="lead">
-		<h3><a target="_blank" href="https://github.com/WangYihang/SniperOJ">Fork/Star This Repositories On Github</a></h3>
-	</p>
+		<p style="text-align: left; margin-left: 32%;"><strong>简  介 : </strong>&nbsp;&nbsp;&nbsp;一个开源的 CTF 训练系统</p>
+		<p style="text-align: left; margin-left: 32%;"><strong>宗  旨 : </strong>&nbsp;&nbsp;&nbsp;分享 自由 专注</p>
+		<p style="text-align: left; margin-left: 32%;"><strong>祝  愿 : </strong>&nbsp;&nbsp;&nbsp;希望大家能在这个平台上收获到知识 , 感受到成就</p>
+		<p style="text-align: left; margin-left: 32%;"><strong>QQ群 : </strong>&nbsp;&nbsp;&nbsp;514782018</p>
+		<p style="text-align: left; margin-left: 32%;"><strong>邮  箱 : </strong>&nbsp;&nbsp;&nbsp;admin@sniperoj.cn</p>
+		<p style="text-align: right;margin-right: 24%;">一航</p>
+		<p style="text-align: right;margin-right: 24%;">于 2017 年 4 月 25 日深夜</p>
 </div>
