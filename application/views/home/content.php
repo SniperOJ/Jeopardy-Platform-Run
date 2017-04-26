9f78b86367130d1e1019a9aa236cf7a69f1bded8
WangYihang <wangyihanger@gmail.com>
2017-04-26 14:37:16 +0800

    add space

--- application/views/home/content.php
+++ application/views/home/content.php
@@ -9,11 +9,11 @@
 <div id="main-scrollbar" class="content-container" data-scrollbar>
 	<h1 class="cover-heading"><span class="hint--right" aria-label="Fork/Star On GitHub"><a target="_blank" href="https://github.com/WangYihang/SniperOJ">SniperOJ</a></span></h1>
 	<p class="lead">
-		<p style="text-align: left; margin-left: 32%;"><strong>简  介 : </strong>&nbsp;&nbsp;&nbsp;一个开源的 CTF 训练系统</p>
-		<p style="text-align: left; margin-left: 32%;"><strong>宗  旨 : </strong>&nbsp;&nbsp;&nbsp;分享 自由 专注</p>
-		<p style="text-align: left; margin-left: 32%;"><strong>祝  愿 : </strong>&nbsp;&nbsp;&nbsp;希望大家能在这个平台上收获到知识 , 感受到成就</p>
+		<p style="text-align: left; margin-left: 32%;"><strong>简&nbsp;&nbsp;介 : </strong>&nbsp;&nbsp;&nbsp;一个开源的 CTF 训练系统</p>
+		<p style="text-align: left; margin-left: 32%;"><strong>宗&nbsp;&nbsp;旨 : </strong>&nbsp;&nbsp;&nbsp;分享 自由 专注</p>
+		<p style="text-align: left; margin-left: 32%;"><strong>祝&nbsp;&nbsp;愿 : </strong>&nbsp;&nbsp;&nbsp;希望大家能在这个平台上收获到知识 , 感受到成就</p>
 		<p style="text-align: left; margin-left: 32%;"><strong>QQ群 : </strong>&nbsp;&nbsp;&nbsp;514782018</p>
-		<p style="text-align: left; margin-left: 32%;"><strong>邮  箱 : </strong>&nbsp;&nbsp;&nbsp;admin@sniperoj.cn</p>
+		<p style="text-align: left; margin-left: 32%;"><strong>邮&nbsp;&nbsp;箱 : </strong>&nbsp;&nbsp;&nbsp;admin@sniperoj.cn</p>
 		<p style="text-align: right;margin-right: 24%;">一航</p>
 		<p style="text-align: right;margin-right: 24%;">于 2017 年 4 月 25 日</p>
 </div>
