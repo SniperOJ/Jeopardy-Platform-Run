d8ea3d5f385af608430a9deeb403cb4726a2488d
WangYihang <wangyihanger@gmail.com>
2017-04-28 02:33:31 +0800

    bug

--- application/views/reset/reset.php
+++ application/views/reset/reset.php
@@ -1,6 +1,8 @@
 <script type="text/javascript">
-    // set form hidden field as reset code
-    $("#reset-code-input").value(<?php echo $reset_code; ?>);
-    // show this form
-    $(form_modal_tab.children('li')[3]).removeAttr("style");
+$(document).ready(function()) {
+	// set form hidden field as reset code
+	$("#reset-code-input").value(<?php echo $reset_code; ?>);
+	// show this form
+	$(form_modal_tab.children('li')[3]).removeAttr("style");
+})
 </script>
