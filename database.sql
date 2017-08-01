f83798f0dcee67979f802ace2fc0a268bf6aec1b
WangYihang <wangyihanger@gmail.com>
2017-08-01 12:30:28 +0800

    [+] Update database.sql

--- database.sql
+++ database.sql
@@ -1,123 +1,164 @@
-CREATE DATABASE `SniperOJ` /*!40100 DEFAULT CHARACTER SET utf8 */
+-- MySQL dump 10.13  Distrib 5.7.18, for Linux (x86_64)
+--
+-- Host: localhost    Database: SniperOJ
+-- ------------------------------------------------------
+-- Server version	5.7.18-0ubuntu0.16.04.1
 
-/* Class */
-CREATE TABLE `users` (
-  `user_id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(16) NOT NULL,
-  `password` varchar(32) NOT NULL,
-  `score` int NOT NULL DEFAULT 0,
-  `college` varchar (64) DEFAULT NULL,
-  `email` varchar(64) DEFAULT NULL,
-  `registe_time` int DEFAULT 0,
-  `registe_ip` varchar(16) not null,
-  `salt` varchar(16) DEFAULT NULL,
-  `active_code` varchar(32) DEFAULT NULL,
-  `active_code_alive_time` varchar(32) DEFAULT NULL,
-  `actived` tinyint NOT NULL DEFAULT 0,
-  `usertype` tinyint NOT NULL DEFAULT 0,
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+--
+-- Table structure for table `captcha`
+--
+
+DROP TABLE IF EXISTS `captcha`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `captcha` (
+  `captcha_id` bigint(13) unsigned NOT NULL AUTO_INCREMENT,
+  `captcha_time` int(10) unsigned NOT NULL,
+  `ip_address` varchar(45) NOT NULL,
+  `word` varchar(20) NOT NULL,
+  PRIMARY KEY (`captcha_id`),
+  KEY `word` (`word`)
+) ENGINE=InnoDB AUTO_INCREMENT=11076 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `challenges`
+--
+
+DROP TABLE IF EXISTS `challenges`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `challenges` (
-  `challenge_id` int NOT NULL AUTO_INCREMENT,
+  `challenge_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(32) NOT NULL,
   `description` varchar(512) NOT NULL,
   `flag` varchar(64) NOT NULL,
-  `score` int NOT NULL DEFAULT 0,
-  `type` ENUM('web', 'pwn', 'stego', 'misc', 'crypto', 'forensics', 'other') DEFAULT 'others',
-  `online_time` int NOT NULL,
-  `visit_times` int DEFAULT 0,
-  `fixing` tinyint DEFAULT 0,
+  `score` int(11) NOT NULL DEFAULT '0',
+  `type` enum('web','pwn','stego','misc','crypto','forensics','other') DEFAULT 'other',
+  `online_time` int(11) NOT NULL,
+  `visit_times` int(11) DEFAULT '0',
+  `fixing` tinyint(4) DEFAULT '0',
   `resource` varchar(512) DEFAULT NULL,
   `document` varchar(512) DEFAULT NULL,
-  `author_id` int NOT NULL DEFAULT 1,
+  `author_id` int(11) NOT NULL DEFAULT '1',
   PRIMARY KEY (`challenge_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE `news` (
-  `news_id` int NOT NULL AUTO_INCREMENT,
-  `title` varchar(64) NOT NULL,
-  `content` varchar(512) NOT NULL,
-  `author_id` int NOT NULL,
-  `submit_time` int NOT NULL DEFAULT 0,
-  `visit_times` int DEFAULT 0,
-  PRIMARY KEY (`news_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+--
+-- Table structure for table `login_log`
+--
 
-CREATE TABLE captcha (
-    captcha_id bigint(13) unsigned NOT NULL auto_increment,
-    captcha_time int(10) unsigned NOT NULL,
-    ip_address varchar(45) NOT NULL,
-    word varchar(20) NOT NULL,
-    PRIMARY KEY `captcha_id` (`captcha_id`),
-    KEY `word` (`word`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-
-/* Verify and Active */
-CREATE TABLE `reset_password` (
-  `reset_id` int NOT NULL AUTO_INCREMENT,
-  `user_id` int NOT NULL,
-  `email` varchar(64) DEFAULT NULL,
-  `reset_code` varchar(32) DEFAULT NULL,
-  `reset_code_alive_time` int DEFAULT 0,
-  `verified` tinyint NOT NULL DEFAULT 0,
-  PRIMARY KEY (`reset_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-
-/* LOG */
-
-CREATE TABLE `register_log`(
-
-)
-
+DROP TABLE IF EXISTS `login_log`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `login_log` (
-  `login_id` int NOT NULL AUTO_INCREMENT,
+  `loginID` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(16) NOT NULL,
   `password` varchar(32) NOT NULL,
-  `login_time` int DEFAULT 0,
-  `login_ip` varchar(16) not null;
-  PRIMARY KEY (`login_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+  `login_time` int(11) DEFAULT '0',
+  `login_ip` varchar(16) NOT NULL,
+  PRIMARY KEY (`loginID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `news`
+--
+
+DROP TABLE IF EXISTS `news`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `news` (
+  `newsID` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(64) NOT NULL,
+  `content` varchar(512) NOT NULL,
+  `authorID` int(11) NOT NULL,
+  `submit_time` int(11) NOT NULL DEFAULT '0',
+  `visit_times` int(11) DEFAULT '0',
+  PRIMARY KEY (`newsID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `reset_password`
+--
+
+DROP TABLE IF EXISTS `reset_password`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `reset_password` (
+  `reset_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `email` varchar(64) DEFAULT NULL,
+  `reset_code` varchar(32) DEFAULT NULL,
+  `reset_code_alive_time` int(11) DEFAULT '0',
+  `verified` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`reset_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `submit_log`
+--
+
+DROP TABLE IF EXISTS `submit_log`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `submit_log` (
-  `submit_id` int NOT NULL AUTO_INCREMENT,
-  `challenge_id`int NOT NULL,
-  `user_id`int NOT NULL,
+  `submit_id` int(11) NOT NULL AUTO_INCREMENT,
+  `challenge_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `flag` varchar(64) NOT NULL,
-  `submit_time` int DEFAULT 0,
-  `is_current` tinyint NOT NULL DEFAULT 0,
+  `submit_time` int(11) DEFAULT '0',
+  `is_current` tinyint(4) DEFAULT '0',
   PRIMARY KEY (`submit_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1363 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `users`
+--
 
-CREATE TABLE `subscribe_log` (
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(16) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `score` int(11) NOT NULL DEFAULT '0',
+  `college` varchar(64) DEFAULT NULL,
+  `email` varchar(64) DEFAULT NULL,
+  `registe_time` int(11) DEFAULT '0',
+  `registe_ip` varchar(16) NOT NULL,
+  `salt` varchar(16) DEFAULT NULL,
+  `usertype` tinyint(4) NOT NULL DEFAULT '0',
+  `actived` tinyint(4) NOT NULL DEFAULT '0',
+  `active_code` varchar(32) DEFAULT NULL,
+  `active_code_alive_time` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=386 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-)
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
-CREATE TABLE `click_log` (
-
-)
-
-
-alter table `challenges` add `author_id` int NOT NULL DEFAULT 1;
-
-alter table `challenges` modify `type` ENUM('web', 'pwn', 'stego', 'misc', 'crypto', 'forensics', 'other') DEFAULT 'other';
-
-alter table `users` add `actived` tinyint NOT NULL DEFAULT 0;
-
-alter table `users` add `active_code` varchar(32) DEFAULT NULL;
-alter table `users` add `active_code_alive_time` varchar(32) DEFAULT NULL;
-
-alter table `users` change column `userID` `user_id` int NOT NULL AUTO_INCREMENT;
-
-alter table `challenges` change column `challengeID` `challenge_id` int NOT NULL AUTO_INCREMENT;
-
-alter table `users` drop column `token`;
-alter table `users` drop column `token_alive_time`;
-alter table `users` drop column `verified`;
-update `users` set actived = 1;
-
-alter table `submit_log` change column `challengeID` `challenge_id` int NOT NULL;
-alter table `submit_log` change column `userID` `user_id` int NOT NULL;
-alter table `submit_log` change column `submitID` `submit_id` int NOT NULL AUTO_INCREMENT;
-
+-- Dump completed on 2017-08-01 12:25:26
